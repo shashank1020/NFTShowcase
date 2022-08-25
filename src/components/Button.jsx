a2314e35c4e2b2e38ccdf2b551e953f38228290c
Shashank <shashankmaurya1020@gmail.com>
2022-08-25 13:40:54 +0530

    expo button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Button = (props) => (
-    <>
-        button
-    </>
-);
+import styles from '../styles/Global';
 
-export default Button;
+const Button = ({ assetUrl, link }) => {
+    return (
+        <div
+            className={styles.btnBlack}
+            onClick={() => window.open(link, "_blank")}
+        >
+            <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+            <div className="flex flex-col justify-start ml-4">
+                <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+                <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+            </div>
+        </div>
+    )
+}
+
+export default Button
